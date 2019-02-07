22d8fa72ca9210bc691c6ca8dc18b105f36379a5
jeandat <jean.dat@gmail.com>
2019-02-07 07:27:25 +0100

    test: check a few cases

--- src/ng-new/index_spec.ts
+++ src/ng-new/index_spec.ts
@@ -2,15 +2,39 @@ import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 
+const name = 'foo';
+const title = 'FOO';
 
 const collectionPath = path.join(__dirname, '../collection.json');
-
+const runner = new SchematicTestRunner('angular-shell-schematic', collectionPath);
 
 describe('angular-shell-schematic', () => {
-  it('works', () => {
-    const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = runner.runSchematic('angular-shell-schematic', {}, Tree.empty());
 
-    expect(tree.files).toEqual([]);
-  });
+    let projectTree:Tree;
+
+    beforeEach(() => {
+        projectTree = Tree.empty();
+    });
+
+    it('should generate tree', () => {
+        const tree = runner.runSchematic('ng-new', {name, title}, projectTree);
+        expect(tree.exists(`${name}/angular.json`)).toBe(true);
+    });
+
+    it('should insert title in README', () => {
+        const tree = runner.runSchematic('ng-new', {name, title}, projectTree);
+        const readmeFile = tree.read(`${name}/README.md`);
+        expect(readmeFile).not.toBeNull();
+        // @ts-ignore
+        expect(readmeFile.toString('utf8')).toMatch(title);
+    });
+
+    it('should insert name in package.json', () => {
+        const tree = runner.runSchematic('ng-new', {name, title}, projectTree);
+        const packageFile = tree.read(`${name}/package.json`);
+        expect(packageFile).not.toBeNull();
+        // @ts-ignore
+        expect(packageFile.toString('utf8')).toMatch(name);
+    });
+
 });
