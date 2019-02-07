4d2697a49222724f84729f74348704cde14c0d8e
jeandat <jean.dat@gmail.com>
2019-02-07 17:26:50 +0100

    chore: exclude the whole template folder from .gitignore

---
+++ src/ng-new/files/__directory__/src/karma.conf.js
@@ -0,0 +1,46 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+const chromeBin = require('puppeteer').executablePath();
+console.log('Using CHROME_BIN:', chromeBin);
+process.env.CHROME_BIN = chromeBin;
+
+module.exports = function(config) {
+    config.set({
+        basePath:'',
+        frameworks:['jasmine', '@angular-devkit/build-angular', 'jasmine-matchers'],
+        plugins:[
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('karma-jasmine-html-reporter'),
+            require('karma-coverage-istanbul-reporter'),
+            require('puppeteer'),
+            require('karma-jasmine-matchers'),
+            require('@angular-devkit/build-angular/plugins/karma')
+        ],
+        files:[
+            
+        ],
+        client:{
+            clearContext:false // leave Jasmine Spec Runner output visible in browser
+        },
+        coverageIstanbulReporter:{
+            dir:require('path').join(__dirname, '../coverage/<%= name %>'),
+            reports:['html', 'lcovonly', 'text-summary'],
+            fixWebpackSourcePaths:true
+        },
+        reporters:['progress', 'kjhtml'],
+        port:9876,
+        colors:true,
+        logLevel:config.LOG_INFO,
+        autoWatch:true,
+        browsers:['Chrome', 'MyHeadlessChrome'],
+        singleRun:false,
+        customLaunchers:{
+            MyHeadlessChrome:{
+                base:'ChromeHeadless',
+                flags:['--disable-translate', '--disable-extensions', '--no-sandbox']
+            }
+        }
+
+    });
+};
