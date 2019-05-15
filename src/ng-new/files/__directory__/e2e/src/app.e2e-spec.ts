05b031c5ac54509ec7e7dc685208fec7dd78952f
Jean DAT <jean.dat@gmail.com>
2019-05-15 20:07:03 +0200

    feat(angular): migration to angular cli 7.3.9

--- src/ng-new/files/__directory__/e2e/src/app.e2e-spec.ts
+++ src/ng-new/files/__directory__/e2e/src/app.e2e-spec.ts
@@ -2,22 +2,27 @@ import { browser, logging } from 'protractor';
 import { AppPage } from './app.po';
 
 describe('<%= name %> app', () => {
-    let page:AppPage;
+  let page: AppPage;
 
-    beforeEach(() => {
-        page = new AppPage();
-    });
+  beforeEach(() => {
+    page = new AppPage();
+  });
 
-    afterEach(async () => {
-        // Assert that there are no errors emitted from the browser
-        const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-        expect(logs).not.toContain(jasmine.objectContaining({
-            level:logging.Level.SEVERE,
-        }));
-    });
+  afterEach(async () => {
+    // Assert that there are no errors emitted from the browser
+    const logs = await browser
+      .manage()
+      .logs()
+      .get(logging.Type.BROWSER);
+    expect(logs).not.toContain(
+      jasmine.objectContaining({
+        level: logging.Level.SEVERE
+      } as logging.Entry)
+    );
+  });
 
-    it('should display welcome message', () => {
-        page.navigateTo();
-        expect(page.getTitle()).toEqual('<%= title %>');
-    });
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getTitle()).toEqual('<%= title %>');
+  });
 });
--- src/ng-new/files/__directory__/src/karma.conf.js
+++ src/ng-new/files/__directory__/src/karma.conf.js
@@ -5,42 +5,40 @@ console.log('Using CHROME_BIN:', chromeBin);
 process.env.CHROME_BIN = chromeBin;
 
 module.exports = function(config) {
-    config.set({
-        basePath:'',
-        frameworks:['jasmine', '@angular-devkit/build-angular', 'jasmine-matchers'],
-        plugins:[
-            require('karma-jasmine'),
-            require('karma-chrome-launcher'),
-            require('karma-jasmine-html-reporter'),
-            require('karma-coverage-istanbul-reporter'),
-            require('puppeteer'),
-            require('karma-jasmine-matchers'),
-            require('@angular-devkit/build-angular/plugins/karma')
-        ],
-        files:[
-            
-        ],
-        client:{
-            clearContext:false // leave Jasmine Spec Runner output visible in browser
-        },
-        coverageIstanbulReporter:{
-            dir:require('path').join(__dirname, '../coverage/<%= name %>'),
-            reports:['html', 'lcovonly', 'text-summary'],
-            fixWebpackSourcePaths:true
-        },
-        reporters:['progress', 'kjhtml'],
-        port:9876,
-        colors:true,
-        logLevel:config.LOG_INFO,
-        autoWatch:true,
-        browsers:['MyHeadlessChrome'],
-        singleRun:false,
-        customLaunchers:{
-            MyHeadlessChrome:{
-                base:'ChromeHeadless',
-                flags:['--disable-translate', '--disable-extensions', '--no-sandbox']
-            }
-        }
-
-    });
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular', 'jasmine-matchers'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('puppeteer'),
+      require('karma-jasmine-matchers'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    files: [],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, '../coverage/<%= name %>'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['MyHeadlessChrome'],
+    singleRun: false,
+    restartOnFileChange: true,
+    customLaunchers: {
+      MyHeadlessChrome: {
+        base: 'ChromeHeadless',
+        flags: ['--disable-translate', '--disable-extensions', '--no-sandbox']
+      }
+    }
+  });
 };
